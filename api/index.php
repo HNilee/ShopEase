8c59aed665ae1b2cf2a81c72889042595fc3810c
HNilee <aryapann12@gmail.com>
2026-04-23 03:04:42 +0700

    Update 11

--- api/index.php
+++ api/index.php
@@ -7,6 +7,7 @@
 // Supresi warning deprecation PHP 8.4+ agar tidak merusak header HTTP
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 ini_set('display_errors', '0');
+ini_set('log_errors', '1');
 
 // Forward request ke Laravel public index.php
 require __DIR__ . '/../public/index.php';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,13 +19,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->composer('*', function ($view) {
-            if (auth()->check()) {
-                $cartQuantity = \App\Models\Cart::where('user_id', auth()->id())->sum('quantity');
-                $view->with('cartQuantity', $cartQuantity);
-            } else {
-                $view->with('cartQuantity', 0);
-            }
-        });
-    }
+    view()->composer('*', function ($view) {
+        try {
+            $cartQuantity = auth()->check()
+                ? \App\Models\Cart::where('user_id', auth()->id())->sum('quantity')
+                : 0;
+        } catch (\Exception $e) {
+            $cartQuantity = 0;
+        }
+        $view->with('cartQuantity', $cartQuantity);
+    });
+}
 }
