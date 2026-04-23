90e910f7a32a012d488f60dac107aa8c1fdf89cf
HNilee <aryapann12@gmail.com>
2026-04-23 19:57:55 +0700

    ENVs

--- api/index.php
+++ api/index.php
@@ -9,5 +9,10 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
 
+$configCache = '/tmp/config.php';
+if (file_exists($configCache)) {
+    unlink($configCache);
+}
+
 // Forward request ke Laravel public index.php
 require __DIR__ . '/../public/index.php';
